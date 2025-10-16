da1f49ef35fa6a6ac2426fdae6534ffde780f6b6
anahaiyang <100533764@alumnos.uc3m.es>
2025-10-16 19:04:16 +0200

    E2E module changes

--- ism/mainIsm.py
+++ ism/mainIsm.py
@@ -5,8 +5,8 @@ from ism.src.ism import ism
 
 # Directory - this is the common directory for the execution of the E2E, all modules
 auxdir = r'D:\MASTER UC3M\EARTH OBSERVATION DATA PROCESSING\GITHUB_EODP\EODP_test\auxiliary'
-indir = r"D:\MASTER UC3M\EARTH OBSERVATION DATA PROCESSING\EODP_TER_2021\EODP-TS-ISM\input\gradient_alt100_act150" # small scene
-outdir = r"D:\MASTER UC3M\EARTH OBSERVATION DATA PROCESSING\EODP_TER_2021\EODP-TS-ISM\myoutput"
+indir = r"D:\MASTER UC3M\EARTH OBSERVATION DATA PROCESSING\EODP_TER_2021\EODP-TS-E2E\sgm_out" # small scene
+outdir = r"D:\MASTER UC3M\EARTH OBSERVATION DATA PROCESSING\EODP_TER_2021\EODP-TS-E2E\myoutput_ism"
 
 # Initialise the ISM
 myIsm = ism(auxdir, indir, outdir)
--- l1b/mainL1b.py
+++ l1b/mainL1b.py
@@ -5,8 +5,8 @@ from l1b.src.l1b import l1b
 
 # Directory - this is the common directory for the execution of the E2E, all modules
 auxdir = r'D:\MASTER UC3M\EARTH OBSERVATION DATA PROCESSING\GITHUB_EODP\EODP_test\auxiliary'
-indir = r"D:\MASTER UC3M\EARTH OBSERVATION DATA PROCESSING\EODP_TER_2021\EODP-TS-L1B\input"
-outdir = r"D:\MASTER UC3M\EARTH OBSERVATION DATA PROCESSING\EODP_TER_2021\EODP-TS-L1B\myoutputs_noeq"
+indir = r"D:\MASTER UC3M\EARTH OBSERVATION DATA PROCESSING\EODP_TER_2021\EODP-TS-E2E\myoutput_ism"
+outdir = r"D:\MASTER UC3M\EARTH OBSERVATION DATA PROCESSING\EODP_TER_2021\EODP-TS-E2E\myoutputs_noeq_l1b"
 
 # Initialise the ISM
 myL1b = l1b(auxdir, indir, outdir)
